c0868d972a4aac02bc97806de420b1a4e841b0f1
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-07-07 18:27:26 +0900

    メール送信機能作成

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,16 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    <p>下記問い合わせがきています。</p>
+
+    <p>お名前:{{ $name }}</p>
+    <p>メールアドレス:{{ $email }}</p>
+
+    <p>問い合わせ内容:</p>
+    <p>{{ $text }}</p>
+</body>
+</html>
