ad1d7af0fdcfd5979eca0fa697a01a68d4780333
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-07-09 10:43:21 +0900

    バリデーション追加

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -10,6 +10,11 @@ class ContactsController extends Controller
 {
     public function sendMail(Request $request)
     {
+        $request->validate([
+            "name" => ["required", "string"],
+            "email" => ["required","email"],
+            "text" => ["required", "string"]
+        ]);
         $name = $request->name;
         $email = $request->email;
         $text = $request->text;
