1e06136ac71646873c7587e4380c4da5483b6744
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-07-11 16:53:59 +0900

    urlカラム追加

--- database/migrations/2021_07_07_081624_create_portfolios_table.php
+++ database/migrations/2021_07_07_081624_create_portfolios_table.php
@@ -20,6 +20,7 @@ class CreatePortfoliosTable extends Migration
             $table->string("github_front")->nullable();
             $table->string("github_api")->nullable();
             $table->string("created");
+            $table->string("url");
             $table->text("detail");
             $table->text("difficulties");
             $table->text("solutions");
